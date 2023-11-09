678bcfe03675e11ec0651d10ebd99db4be02c30e
Cherry on a cake <100362071+SirYefever@users.noreply.github.com>
2023-11-09 14:54:47 +0700

    Uploaded dub folder

---
+++ dist/code.py
@@ -0,0 +1,13 @@
+
+from matplotlib import pyplot as plt
+from operator import itemgetter
+
+f = open("x and y values.txt").readlines()
+data = [tuple(map(float, line.split())) for line in f]
+x = list(map(itemgetter(0), data))
+y = list(map(itemgetter(1), data))
+
+#ploting our canvas
+plt.plot(y, x)
+#display the graph
+plt.show()
